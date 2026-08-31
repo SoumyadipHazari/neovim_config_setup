cc2237b1bd489a9157c197a745c22224fd2141df
SoumyadipHazari <166205459+SoumyadipHazari@users.noreply.github.com>
2026-08-31 20:51:59 +0530

    added lua folder

---
+++ lua/core/keymaps.lua
@@ -0,0 +1,12 @@
+-- keymaps
+
+-- set leader key
+
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+-- save file
+vim.keymap.set('n', '<C-s>', '<cmd> q <CR>', opts)
+
+-- window management
+-- vim.keymap.set
