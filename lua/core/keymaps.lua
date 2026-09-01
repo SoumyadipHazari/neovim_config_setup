95f8552ac0ab30fdd9a7526ff34e9806bc82df20
SoumyadipHazari <166205459+SoumyadipHazari@users.noreply.github.com>
2026-09-01 20:32:56 +0530

    fixed colorscheme

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -6,7 +6,6 @@ vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
 -- save file
-vim.keymap.set('n', '<C-s>', '<cmd> q <CR>', opts)
 
 -- window management
 -- vim.keymap.set
--- lua/plugins/colortheme.lua
+++ lua/plugins/colortheme.lua
@@ -1,6 +1,15 @@
-return{
-	"folke/tokyonight.nvim",
-  	lazy = false,
-  	priority = 1000,
-  	opts = {}
+return {
+  "catppuccin/nvim",
+  name = "catppuccin",
+  priority = 1000,
+
+  opts = {
+    flavour = "mocha",
+    transparent_background = true
+  },
+
+  config = function(_, opts)
+    require("catppuccin").setup(opts)
+    vim.cmd.colorscheme("catppuccin")
+  end,
 }
