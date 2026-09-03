38884523a2c3f086e2d1b87ccb0be7da7cfcb5b3
SoumyadipHazari <166205459+SoumyadipHazari@users.noreply.github.com>
2026-09-04 00:59:08 +0530

    fix: resize

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -16,10 +16,10 @@ vim.keymap.set('n', 'n', 'nzzzv')
 vim.keymap.set('n', 'N', 'Nzzzv')
 
 -- resize with the arrows
-vim.keymap.set('n', '<Up>', ':resize -2<CR>', opts)
-vim.keymap.set('n', '<Down>', ':resize +2<CR>', opts)
-vim.keymap.set('n', '<Left>', ':vertical resize -2<CR>', opts)
-vim.keymap.set('n', '<Right>', ':vertical resize +2<CR>', opts)
+vim.keymap.set('n', '<S-h>', ':resize -2<CR>', opts)
+--vim.keymap.set('n', '<S-k>', ':resize +2<CR>', opts)
+--vim.keymap.set('n', '<S-j>', ':vertical resize -2<CR>', opts)
+vim.keymap.set('n', '<S-l>', ':vertical resize +2<CR>', opts)
 
 
 -- window management
@@ -27,6 +27,12 @@ vim.keymap.set('n', '<leader>v', '<C-w>v', opts) --vertical splitting
 vim.keymap.set('n', '<leader>h', '<C-w>s', opts) -- horizontal splitting
 vim.keymap.set('n', '<leader>se', '<C-w>=', opts) -- equal width and height splitting
 vim.keymap.set('n', '<leader>xs', ':close<CR>', opts) -- close current split
+
+--tab switching
+vim.keymap.set('n', '<C-h>', '<C-w>h', opts) -- move left split
+vim.keymap.set('n', '<C-j>', '<C-w>j', opts) -- move bottom split
+vim.keymap.set('n', '<C-k>', '<C-w>k', opts) -- move top split
+vim.keymap.set('n', '<C-l>', '<C-w>l', opts) -- move right split
 
 --tabs
 vim.keymap.set('n', '<leader>to', ':tabnew<CR>', opts) -- new tab
