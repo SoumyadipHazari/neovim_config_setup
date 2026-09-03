a2d35a8c71e9a8b2a945b0c5e84150ff56378b58
SoumyadipHazari <166205459+SoumyadipHazari@users.noreply.github.com>
2026-09-03 23:16:01 +0530

    feat: added keymaps

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -7,5 +7,31 @@ vim.g.maplocalleader = ' '
 
 -- save file
 
+--vertical scroll and center
+vim.keymap.set('n', '<C-d>', '<C-d>zz', opts)
+vim.keymap.set('n', '<C-u>', '<C-u>zz', opts)
+
+-- search navigation
+vim.keymap.set('n', 'n', 'nzzzv')
+vim.keymap.set('n', 'N', 'Nzzzv')
+
+-- resize with the arrows
+vim.keymap.set('n', '<Up>', ':resize -2<CR>', opts)
+vim.keymap.set('n', '<Down>', ':resize +2<CR>', opts)
+vim.keymap.set('n', '<Left>', ':vertical resize -2<CR>', opts)
+vim.keymap.set('n', '<Right>', ':vertical resize +2<CR>', opts)
+
+
 -- window management
+vim.keymap.set('n', '<leader>v', '<C-w>v', opts) --vertical splitting
+vim.keymap.set('n', '<leader>h', '<C-w>s', opts) -- horizontal splitting
+vim.keymap.set('n', '<leader>se', '<C-w>=', opts) -- equal width and height splitting
+vim.keymap.set('n', '<leader>xs', ':close<CR>', opts) -- close current split
+
+--tabs
+vim.keymap.set('n', '<leader>to', ':tabnew<CR>', opts) -- new tab
+vim.keymap.set('n', '<leader>tx', ':tabclose<CR>', opts) -- close current tab
+vim.keymap.set('n', '<leader>tn', ':tabn<CR>', opts) -- next tab
+vim.keymap.set('n', '<leader>tp', ':tabp<CR>', opts) -- previous tab
+
 -- vim.keymap.set
