ebd7b28522ad7f809b0d40aebd7a3f34bd0331ae
SoumyadipHazari <166205459+SoumyadipHazari@users.noreply.github.com>
2026-09-05 19:06:22 +0530

    feat: added languages

--- lua/plugins/tree.lua
+++ lua/plugins/tree.lua
@@ -4,28 +4,7 @@ return {
   build = ':TSUpdate',
   config = function()
     local parsers = {
-        'bash',
-        'cmake',
-        'css',
-        'dockerfile',
-        'gitignore',
-        'go',
-        'graphql',
-        'html',
-        'java',
-        'javascript',
-        'react',
-        'lua',
-        'json',
-        'sql',
-        'toml',
-        'typescript',
-        'vim',
-        'python',
-        'yaml',
-        'markdown',
-        'markdown_inline',
-        'vue',
+        'bash','cmake','css','dockerfile','gitignore','go','graphql','html','java','javascript','lua','json','sql','toml','typescript','vim','python','yaml','markdown','markdown_inline','vue','zig','ruby','regex', 'rasi', 'nix','nginx', 'latex', 'kotlin', 'javadoc', 'jinja_inline', 'jinja', 'http', 'haskell', 'awk', 'cpp', 'cuda', 'dot' 
     }
     local ts = require 'nvim-treesitter'
     ts.install(parsers)
