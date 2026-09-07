ff9078a5f55b681f5e7507ab2bef77093eb24723
SoumyadipHazari <166205459+SoumyadipHazari@users.noreply.github.com>
2026-09-08 01:42:00 +0530

    update: added mason-lsp

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -3,8 +3,30 @@ return {
     opts = {
         ui = {
             icons = {
-                package_installed = ""
+                package_installed = "✓",
+                package_pending = "➜",
+                package_uninstalled = "✗"
             }
         }
-    }
+    },
+    {
+      "mason-org/mason-lspconfig.nvim",
+      opts = {
+        ensure_installed = {
+            "lua_ls", "pyright", "ts_ls", "rust_analyzer", "clangd"
+        },
+      },
+    },
+    {
+        "neovim/nvim-lspconfig",
+        config = function()
+            local lspconfig = require("lspconfig")
+
+            lspconfig.lua_ls.setup({})
+            lspconfig.pyright.setup({})
+            lspconfig.ts_ls.setup({})
+            lspconfig.rust_analyzer({})
+            lspconfig.clangd({})
+        end
+    },
 }
